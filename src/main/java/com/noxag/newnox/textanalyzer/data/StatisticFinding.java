fabad3a9ea27efdc26d3f0e8a0513c1dcd0540e8
tobiasxschmidt <tsbaunatal@gmail.com>
2017-09-19 12:02:50 +0200

    Added textanalyzer data models (template only)

---
+++ src/main/java/com/noxag/newnox/textanalyzer/data/StatisticFinding.java
@@ -0,0 +1,5 @@
+package com.noxag.newnox.textanalyzer.data;
+
+public class StatisticFinding extends Finding {
+
+}
---
+++ src/main/java/com/noxag/newnox/textanalyzer/data/TextFinding.java
@@ -0,0 +1,5 @@
+package com.noxag.newnox.textanalyzer.data;
+
+public class TextFinding extends Finding {
+
+}
