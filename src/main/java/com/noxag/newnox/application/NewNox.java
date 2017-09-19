35debe308db80958ba52d39c475399e5c8cce712
tobiasxschmidt <tsbaunatal@gmail.com>
2017-09-19 11:57:12 +0200

    Format improvement

--- src/main/java/com/noxag/newnox/application/NewNox.java
+++ src/main/java/com/noxag/newnox/application/NewNox.java
@@ -3,11 +3,9 @@ package com.noxag.newnox.application;
 import com.noxag.newnox.ui.NewNoxWindow;
 
 public class NewNox {
-
     public static void main(String[] args) {
         NewNoxWindow mainWindow = new NewNoxWindow();
         mainWindow.setBounds(600, 150, 800, 600);
         mainWindow.setVisible(true);
     }
-
 }
