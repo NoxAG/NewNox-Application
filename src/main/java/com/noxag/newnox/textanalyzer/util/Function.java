a8bc9d3743580232738a3a7c516df0d02a5b751e
tobiasxschmidt <tsbaunatal@gmail.com>
2017-10-04 16:46:18 +0200

    Added Java Doc to Function Interface

--- src/main/java/com/noxag/newnox/textanalyzer/util/Function.java
+++ src/main/java/com/noxag/newnox/textanalyzer/util/Function.java
@@ -2,6 +2,21 @@ package com.noxag.newnox.textanalyzer.util;
 
 import java.io.IOException;
 
+/**
+ * This interface is used to represent a Function with three input parameters
+ * and one result
+ * 
+ * @author Tobias.Schmidt@de.ibm.com
+ *
+ * @param <A>
+ *            the first parameter type of the function
+ * @param <B>
+ *            the second parameter type of the function
+ * @param <C>
+ *            the third parameter type of the function
+ * @param <D>
+ *            the result type of the function
+ */
 @FunctionalInterface
 public interface Function<A, B, C, D> {
     public D apply(A one, B two, C three) throws IOException;
