de3d5b2a39ece4d7b1c4129dd9fa0bb57d3d5df2
tobiasxschmidt <tsbaunatal@gmail.com>
2017-10-04 16:40:28 +0200

    Added Java Doc to Textanalyzer

--- src/main/java/com/noxag/newnox/textanalyzer/Textanalyzer.java
+++ src/main/java/com/noxag/newnox/textanalyzer/Textanalyzer.java
@@ -8,6 +8,12 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 
 import com.noxag.newnox.textanalyzer.data.Finding;
 
+/**
+ * This class is used to run a set of {@link TextanalyzerAlgorithm}
+ * 
+ * @author Tobias.Schmidt@de.ibm.com
+ *
+ */
 public class Textanalyzer {
     List<Function<PDDocument, List<Finding>>> algorithms;
 
@@ -15,6 +21,14 @@ public class Textanalyzer {
         this.algorithms = algorithms;
     }
 
+    /**
+     * This method runs all {@link TextanalyzerAlgorithm} contained in this
+     * class and merges the results of all analysis
+     * 
+     * @param pdfDoc
+     *            the PDF document to be analyzed
+     * @return the results of all analysis
+     */
     public List<Finding> analyze(PDDocument pdfDoc) {
         List<Finding> findings = new ArrayList<>();
         algorithms.stream().forEach(algorithm -> findings.addAll((algorithm.apply(pdfDoc))));
