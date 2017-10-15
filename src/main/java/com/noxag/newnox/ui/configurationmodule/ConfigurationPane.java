113812e6013b6c56246e0188319573b129a802f9
luciehopp <32615689+luciehopp@users.noreply.github.com>
2017-10-15 15:40:04 +0200

    Nur noch Buttons, kein Textfield. Methode getRunButton(), die den Run Button übergibt.

--- src/main/java/com/noxag/newnox/ui/configurationmodule/ConfigurationPane.java
+++ src/main/java/com/noxag/newnox/ui/configurationmodule/ConfigurationPane.java
@@ -2,9 +2,9 @@ package com.noxag.newnox.ui.configurationmodule;
 
 import java.util.List;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
@@ -12,31 +12,41 @@ import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class ConfigurationPane extends BorderPane {
+
+    public Button btnrun, open;
+
     public ConfigurationPane() {
-        this.setCenter(new ConfigurationTabPane());
+
+        ConfigurationTabPane configTabPane = new ConfigurationTabPane();
+        this.setCenter(configTabPane);
 
         this.setBorder(new Border(
                 new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
         // Add "run" button
-        Button btnrun = new Button("Run");
-        VBox btnbox = new VBox();
-        this.setRight(btnbox);
-        btnbox.getChildren().add(btnrun);
-        btnbox.setAlignment(Pos.CENTER_RIGHT);
+        btnrun = new Button("Run");
+        open = new Button("Open File ...");
+        HBox btnbox = new HBox();
+        this.setBottom(btnbox);
+        btnbox.getChildren().addAll(open, btnrun);
+        btnbox.setAlignment(Pos.BOTTOM_RIGHT);
+        HBox.setMargin(btnrun, new Insets(4.0, 4.0, 4.0, 4.0));
+        HBox.setMargin(open, new Insets(4.0, 0.0, 4.0, 4.0));
+        // btnrun.minWidthProperty().bind(open.widthProperty());
 
-        // Textfield Path
-        TextField path = new TextField();
-        HBox hb = new HBox();
-        this.setBottom(hb);
-        Button open = new Button("Open");
-        hb.getChildren().addAll(path, open);
-        HBox.setHgrow(path, Priority.ALWAYS);
+        /*
+         * FileChooser fileChooser = new FileChooser();
+         * fileChooser.setTitle("Open File...");
+         */
 
+    }
+
+    public Button getRunButton() {
+        // Gibt Run Button zurück
+        return btnrun;
     }
 
     public List<String> getTextanalyzer() {
