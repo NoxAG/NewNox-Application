1a64c4c55121f5e78b2fff6f48c339a4db63dda3
luciehopp <32615689+luciehopp@users.noreply.github.com>
2017-10-15 14:28:42 +0200

    Run Button und Textfield für das Configuration Module

--- src/main/java/com/noxag/newnox/ui/configurationmodule/ConfigurationPane.java
+++ src/main/java/com/noxag/newnox/ui/configurationmodule/ConfigurationPane.java
@@ -2,12 +2,18 @@ package com.noxag.newnox.ui.configurationmodule;
 
 import java.util.List;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class ConfigurationPane extends BorderPane {
@@ -17,6 +23,20 @@ public class ConfigurationPane extends BorderPane {
         this.setBorder(new Border(
                 new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         // Add "run" button
+        Button btnrun = new Button("Run");
+        VBox btnbox = new VBox();
+        this.setRight(btnbox);
+        btnbox.getChildren().add(btnrun);
+        btnbox.setAlignment(Pos.CENTER_RIGHT);
+
+        // Textfield Path
+        TextField path = new TextField();
+        HBox hb = new HBox();
+        this.setBottom(hb);
+        Button open = new Button("Open");
+        hb.getChildren().addAll(path, open);
+        HBox.setHgrow(path, Priority.ALWAYS);
+
     }
 
     public List<String> getTextanalyzer() {
--- src/main/java/com/noxag/newnox/ui/configurationmodule/ConfigurationTab.java
+++ src/main/java/com/noxag/newnox/ui/configurationmodule/ConfigurationTab.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
 
 public class ConfigurationTab extends Tab {
     public ConfigurationTab(String name) {
@@ -13,9 +14,24 @@ public class ConfigurationTab extends Tab {
     public ConfigurationTab(String name, List<String> AlgorithmUINames) {
         this.setText(name);
         // TODO:add checkboxes for algorithms
+
     }
 
     public void addAlgorithms(List<String> AlgorithmUINames) {
+        HBox hbox = new HBox();
+        this.setContent(hbox);
+
+        /*
+         * CheckBox hb = new CheckBox(); hb.setText("First");
+         * hb.setSelected(false); hbox.getChildren().add(hb); //
+         * AlgorithmUINames.get(i)
+         */
+
         // TODO: add checkboxes for algorithms
+        /*
+         * for (int i = 0; i < AlgorithmUINames.size(); i++) {
+         * hbox.getChildren().add(new CheckBox(AlgorithmUINames.get(i))); }
+         */
+
     }
 }
