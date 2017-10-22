f016f3d185f30889418181928f6900048a0b7d5d
tobiasxschmidt <tsbaunatal@gmail.com>
2017-10-22 13:36:49 +0200

    fixed issue #92

--- src/main/java/com/noxag/newnox/textanalyzer/data/pdf/PDFLine.java
+++ src/main/java/com/noxag/newnox/textanalyzer/data/pdf/PDFLine.java
@@ -63,4 +63,10 @@ public class PDFLine implements PDFObject {
         }
     }
 
+    @Override
+    public String toString() {
+        return words.stream().map(TextPositionSequence::toString).reduce(String::concat)
+                .orElseGet(() -> super.toString());
+    }
+
 }
